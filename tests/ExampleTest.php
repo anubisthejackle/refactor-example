2acc00f97b430b8b4f9301785449f92a36f1fc21
Travis Weston <tweston2013@gmail.com>
2021-01-24 20:48:34 -0500

    Add passing monday failure path test with mailCounter

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -22,8 +22,11 @@ namespace Legacy {
     }
 
     function exec() {}
+
+    $mailCounter = 0;
     function mail() {
-        assertTrue(true);
+        global $mailCounter;
+        $mailCounter++;
     }
 }
 
@@ -32,7 +35,9 @@ namespace Tests {
         public function setUp() : void {
             global $fileExistsReturnValue;
             global $dateReturnValue;
+            global $mailCounter;
 
+            $mailCounter = 0;
             $fileExistsReturnValue = true;
             $dateReturnValue = 'MON';
         }
@@ -45,9 +50,11 @@ namespace Tests {
 
         public function test_monday_script_runs_as_expected_when_file_does_not_exist() {
             global $fileExistsReturnValue;
+            global $mailCounter;
+
             $fileExistsReturnValue = false;
             include( dirname(__DIR__) . '/scripts/legacyScript.php' );
-
+            $this->assertEquals(1, $mailCounter);
         }
 
         public function test_monday_script_runs_as_expected_when_file_does_not_exist_but_does_download() {
@@ -65,13 +72,13 @@ namespace Tests {
             include( dirname(__DIR__) . '/scripts/legacyScript.php' );
         }
 
-        public function test_friday_script_runs_as_expected_when_file_does_not_exist() {
-            global $fileExistsReturnValue;
-            global $dateReturnValue;
-            $dateReturnValue = 'FRI';
-            $fileExistsReturnValue = false;
+        // public function test_friday_script_runs_as_expected_when_file_does_not_exist() {
+        //     global $fileExistsReturnValue;
+        //     global $dateReturnValue;
+        //     $dateReturnValue = 'FRI';
+        //     $fileExistsReturnValue = false;
             
-            include( dirname(__DIR__) . '/scripts/legacyScript.php' );
-        }
+        //     include( dirname(__DIR__) . '/scripts/legacyScript.php' );
+        // }
     }
 }
