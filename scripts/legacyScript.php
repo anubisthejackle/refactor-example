1511e8fa57adef03a0e12870a2a0a4f05d8f94fb
Travis Weston <tweston2013@gmail.com>
2021-01-24 19:48:15 -0500

    Revert script to original anonymized state

--- scripts/legacyScript.php
+++ scripts/legacyScript.php
@@ -1,7 +1,48 @@
 <?php
-namespace Legacy;
-require_once( __DIR__ . '/functions.php' );
+$dates = array(
+	'MON' => array(
+		729181 => 'tue',
+	),
+	'TUE' => array(
+		729182 => 'wed',
+	),
+	'WED' => array(
+		729183 => 'thu',
+	),
+	'THU' => array(
+		729184 => 'fri',
+	),
+	'FRI' => array(
+		729185 => 'sat',
+		729021 => 'sun',
+		729180 => 'mon',
+	),
+);
 
-$example = new Example();
+$connection = 'ftp://username:password@ftp.example.com/';
 
-$example->getFile();
+foreach( $dates[ strtoupper( date( 'D' ) ) ] as $id => $name ) {
+	$filepath = '/mnt/data/location/' . $id . '.eps';
+
+	$tries = array(
+			strtoupper( $name ) . '.eps',
+			strtoupper( $name ) . '.EPS',
+			$name . '.eps',
+			$name . '.EPS',
+		);
+
+	foreach( $tries as $try ) {
+		$url = $connection . 'bang' . $try;
+
+		if( !file_exists( $filepath ) ) {
+			exec('wget -O ' . str_replace(' ', '\ ', $filepath) . ' ' . $url);
+		}
+
+	}
+
+	if( !file_exists( $filepath ) ) {
+		mail( 'me@email.com', 'File failed to download', 'The file failed to download. You might need to download it manually.' );
+	} else {
+		echo 'Success';
+	}
+}
