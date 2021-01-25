1279a3c7ed2a42c080227875c1ea839e5b7dfde2
Travis Weston <tweston2013@gmail.com>
2021-01-24 19:51:45 -0500

    Add namespacing for tests for step 2

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Legacy {
+    function date() {}
+    function file_exists() {}
+    function exec() {}
+    function mail() {}
+}
+
+namespace Tests {
+    class ExampleTest extends \PHPUnit\Framework\TestCase {
+
+    }
+}
